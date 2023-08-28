0a8b49abafde432719efc691e689a955e2394dd3
Shikigami Maito <shikigamimaito25@gmail.com>
2023-08-28 14:52:08 +0700

    add hide animation on scroll for desktop navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,68 @@
-const currentLanguageDesk = document.querySelector('.language-switcher__current')
-const languageDropBoxDesk = document.querySelector('.language-switcher__btns');
+const currentLanguageDesk = document.querySelector(
+  ".language-switcher__current"
+);
+const languageDropBoxDesk = document.querySelector(".language-switcher__btns");
 
-const currentLanguageMobi = document.querySelector('#language-switcher__current-mobile')
-const languageDropBoxMobi = document.querySelector('#language-switcher__btns-mobile');
+const desktopNavBar = document.querySelector(".nav__wrapper");
 
-const mobileNavBar = document.querySelector('.mobile-nav__main');
-const hamburgerBtn = document.querySelector('#hamburger-btn')
-const navPopup = document.querySelector('.mobile-nav__popup')
-const closePopupBtn = document.querySelector('.popup__close-btn')
-const popupLinks = document.querySelectorAll('.mobile-nav__link')
+const currentLanguageMobi = document.querySelector(
+  "#language-switcher__current-mobile"
+);
+const languageDropBoxMobi = document.querySelector(
+  "#language-switcher__btns-mobile"
+);
 
-currentLanguageDesk.addEventListener('click', (e) => {
-  languageDropBoxDesk.classList.toggle('hide');
-})
+const mobileNavBar = document.querySelector(".mobile-nav__main");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const navPopup = document.querySelector(".mobile-nav__popup");
+const closePopupBtn = document.querySelector(".popup__close-btn");
+const popupLinks = document.querySelectorAll(".mobile-nav__link");
 
-currentLanguageMobi.addEventListener('click', (e) => {
-  languageDropBoxMobi.classList.toggle('hide');
-})
+currentLanguageDesk.addEventListener("click", (e) => {
+  languageDropBoxDesk.classList.toggle("hide");
+});
 
-hamburgerBtn.addEventListener('click', (e) => {
-  navPopup.classList.toggle('hide');
-})
+currentLanguageMobi.addEventListener("click", (e) => {
+  languageDropBoxMobi.classList.toggle("hide");
+});
 
-closePopupBtn.addEventListener('click', (e) => {
-  navPopup.classList.toggle('hide');
-})
+hamburgerBtn.addEventListener("click", (e) => {
+  navPopup.classList.toggle("hide");
+});
+
+closePopupBtn.addEventListener("click", (e) => {
+  navPopup.classList.toggle("hide");
+});
 
 let lastScrollTop = 0;
-window.addEventListener("scroll", function(){ 
-    let st = window.scrollY || document.documentElement.scrollTop; 
-    if (st > lastScrollTop && !mobileNavBar.classList.contains('hide')) {
-      mobileNavBar.classList.add('hide')
-    } else if (st < lastScrollTop && mobileNavBar.classList.contains('hide')) {
-      mobileNavBar.classList.remove('hide')
-    } 
-    lastScrollTop = st <= 0 ? 0 : st; 
-}, false);
+window.addEventListener(
+  "scroll",
+  function () {
+    let st = window.scrollY || document.documentElement.scrollTop;
+    if (
+      st > lastScrollTop &&
+      !(
+        mobileNavBar.classList.contains("hide") ||
+        desktopNavBar.classList.contains("hide")
+      )
+    ) {
+      mobileNavBar.classList.add("hide");
+      desktopNavBar.classList.add("hide");
+    } else if (
+      st < lastScrollTop &&
+      (mobileNavBar.classList.contains("hide") ||
+        desktopNavBar.classList.contains("hide"))
+    ) {
+      mobileNavBar.classList.remove("hide");
+      desktopNavBar.classList.remove("hide");
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  },
+  false
+);
 
-popupLinks.forEach(link => {
-  link.addEventListener('click', (e) => {
-    navPopup.classList.remove('hide')
-  })
-})
+popupLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    navPopup.classList.remove("hide");
+  });
+});
