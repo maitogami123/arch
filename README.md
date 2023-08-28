8c2732ea6698feb481d7aa7aecb5ffaf83e55800
Shikigami Maito <shikigamimaito25@gmail.com>
2023-08-28 10:20:19 +0700

    add basic animation

---
+++ README.md
@@ -0,0 +1,8 @@
+If you already have sass installed run
+```sh
+  npm run sass
+```
+Or install sass by using this command
+```sh
+  npm install --save-dev sass
+```
---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+const currentLanguageDesk = document.querySelector('.language-switcher__current')
+const languageDropBoxDesk = document.querySelector('.language-switcher__btns');
+
+const currentLanguageMobi = document.querySelector('#language-switcher__current-mobile')
+const languageDropBoxMobi = document.querySelector('#language-switcher__btns-mobile');
+
+const mobileNavBar = document.querySelector('.mobile-nav__main');
+const hamburgerBtn = document.querySelector('#hamburger-btn')
+const navPopup = document.querySelector('.mobile-nav__popup')
+const closePopupBtn = document.querySelector('.popup__close-btn')
+const popupLinks = document.querySelectorAll('.mobile-nav__link')
+
+currentLanguageDesk.addEventListener('click', (e) => {
+  languageDropBoxDesk.classList.toggle('hide');
+})
+
+currentLanguageMobi.addEventListener('click', (e) => {
+  languageDropBoxMobi.classList.toggle('hide');
+})
+
+hamburgerBtn.addEventListener('click', (e) => {
+  navPopup.classList.toggle('hide');
+})
+
+closePopupBtn.addEventListener('click', (e) => {
+  navPopup.classList.toggle('hide');
+})
+
+let lastScrollTop = 0;
+window.addEventListener("scroll", function(){ 
+    let st = window.scrollY || document.documentElement.scrollTop; 
+    if (st > lastScrollTop && !mobileNavBar.classList.contains('hide')) {
+      mobileNavBar.classList.add('hide')
+    } else if (st < lastScrollTop && mobileNavBar.classList.contains('hide')) {
+      mobileNavBar.classList.remove('hide')
+    } 
+    lastScrollTop = st <= 0 ? 0 : st; 
+}, false);
+
+popupLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    navPopup.classList.remove('hide')
+  })
+})
